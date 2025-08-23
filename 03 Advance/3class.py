dc0ba060849895730a5564a7d2bdb987b21e80d7
Satya7250 <satyaprakashdh3134@gamil.com>
2025-08-23 10:51:58 +0530

    set in python

---
+++ 03 Advance/3class.py
@@ -0,0 +1,26 @@
+# list = [23,4,5,"Satya",[[1,10],[1,10]],[1,10]]
+# print(list.count([1,10]))
+
+
+# t=(10,20,30,40,10,9j,True,"sk")
+# t=()
+# t=10,20,30,40
+# t=(10,)
+# print(t)
+# print(type(t))
+
+#using eval() function
+# t=eval(input("Enter the num: "))
+# print(type(t))
+# print(t)
+
+# list=[10,20,30]
+# t=tuple(list)
+# print(t)
+# print(type(t))
+
+
+t=(1,2,3,4)
+print(min(t))
+print(max(t))
+
---
+++ 03 Advance/4class.py
@@ -0,0 +1,39 @@
+#example of packing of tuple
+'''a=10
+b=20
+c=30
+d=40
+t=a,b,c,d
+print(t)
+print(type(t))'''
+
+'''t=10,20,30,40
+a,b,c,d=t
+print("a: ",a,"b: ",b,"c: ",c,"d: ",d)'''
+
+#del keyword in python
+'''# x=[1,2,"gh"]
+x=10
+# x=(3,4,5)
+print(x)
+print(type(x))
+del x #x is not define because it deleted from memory
+print(x) '''
+
+#set in python
+'''t = eval(input("Enter the: "))
+print(type(t))
+print(t)'''
+
+#typecasting list in set
+'''l=[10,20,30,40,20,30]
+x=set(l)
+print(x)
+print(type(x))'''
+
+#typecasting tuple in set
+# t=10,
+t="college"
+y=set(t)
+print(y)
+print(type(y))
