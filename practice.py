b7422ec31b5263be56ab07e4fb05da70b0a17922
Satya7250 <satyaprakashdh3134@gamil.com>
2025-11-23 18:32:55 +0530

    problem solving of loop

--- practice.py
+++ practice.py
@@ -82,8 +82,87 @@
 # print(f"you order size is {size} with {message}")
 
 '''Eight problem'''
-year = 2024
-if year%4 ==0 and year%100 != 0 or year%400 == 0:
-    print("leap year")
-else:
-    print("not leap year")
+# year = 2024
+# if year%4 ==0 and year%100 != 0 or year%400 == 0:
+#     print("leap year")
+# else:
+#     print("not leap year")
+
+'''Ninth problem'''
+# list = [1,2,3,-4,-6]
+# count=0
+# for i in list:
+#     if i>=1:
+#         count+=1
+# print(count)
+
+'''10th Problem'''
+# num = 10
+# sum=0
+# for i in range(1,num+1):
+#     if(i%2==0):
+#         sum+=i
+# print(sum)
+
+'''11th problem'''
+# num = 2
+# for i in range(1,11):
+#     if(i==5):
+#         continue
+#     else:
+#         print(i*num)
+
+'''12th problem'''
+# string = "satyaprakash"
+# rev=""
+# for i in string:
+#     rev=i+rev
+# print(rev)
+
+'''13th problem'''
+# string = "satyaprakash"
+# for char in string:
+#     if string.count(char) == 1:
+#         print(char)
+#         break
+
+'''14th problem'''
+# num = 5
+# fact = 1
+# for i in range(1,num+1):
+#     fact = fact*i
+# print(fact)
+
+'''15th problem'''
+# con = 1
+# while con:
+#     num = int(input("Enter the num: "))
+#     for i in range (1,11):
+#         if i == num:
+#           print("number match")
+#           con=0
+#           break
+
+'''16th problem'''
+# num = 7
+# is_prime = True
+# for i in range(2,num):
+#     if num%i == 0:
+#         is_prime = False
+#         break
+# if is_prime == True:
+#     print("prime")
+# else:
+#     print("not prime")
+
+'''17th problem'''
+# item = ["apple","banana","banana","mango"]
+
+# unique_item = set()
+# for i in item:
+#     if i in unique_item:
+#         print("Duplicate: ",i)
+#         break
+#     unique_item.add(i)
+
+'''18th problem'''
